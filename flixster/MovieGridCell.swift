37a53496df5e2760a2c01ca475d5af150182154c
Sanjidah Abdullah <sanjidah42@gmail.com>
2021-02-19 21:44:01 -0500

    Flixster Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Sanjidah Abdullah on 2/19/21.
+//  Copyright © 2021 Sanjidah Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
